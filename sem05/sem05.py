f4b1fd3c31909d9b74e1e4cdba46e999097db44d
mimaboci <michaelabocanova@gmail.com>
2018-01-28 02:16:27 +0100

    sem05 completed

--- sem05/sem05.py
+++ sem05/sem05.py
@@ -1,21 +1,29 @@
 import http.client
-import http.server
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse
 
 class server(BaseHTTPRequestHandler):
     def do_GET(self):
         print("do_GET")
-		query = urlparse(self.path).query
-		
+        query = urlparse(self.path).query
         self.send_response(200, 'ok')
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
-        message = "You sent {}".format(query)
-        self.wfile.write(bytes(message, "utf8"))
-        
+        message = "You asked for {}".format(self.path[1:])
+        self.wfile.write(bytes('<html><head><title>title</title></head>', 'utf8'))
+        self.wfile.write(bytes("<body><p>body</p>", 'utf8'))
+        self.wfile.write(bytes("<p>%s</p>" %message, 'utf8'))
+        self.wfile.write(bytes("</body></html>", 'utf8'))        
 
-address = ('localhost', 8000)
-httpd = HTTPServer(address, server)
-print("server running")
-httpd.serve_forever()
+def start():
+    address = ('localhost', 8000)
+    httpd = HTTPServer(address, server)
+    print("server running")
+    httpd.serve_forever()
+    
+def exit():
+    httpd.server_close()
+    print("server stopped")
+
+start()
