3ff31a222ac5a34465178ce544d60a4f84fb37f3
mimaboci <michaelabocanova@gmail.com>
2018-01-28 02:00:12 +0100

    sem04 completed

--- sem04/sem04.py
+++ sem04/sem04.py
@@ -1,44 +1,78 @@
 import json
-from bokeh.plotting import figure, show
-from bokeh.models import ColumnDataSource, LabelSet, ranges
-from math import pi
+from bokeh.plotting import ColumnDataSource, figure, show
+from bokeh.models import LabelSet, ranges
+from cmath import pi
 
 file = open('election.json')
 data = json.load(file)
-
+    
 names = []
 shares = []
-colors = []
-
+colors = []        
 below_One_sum = 0
+
 for i in data:
     if(i.get('share') < 1):
-        below_One_sum += i.get('share') #sum
+        below_One_sum += i.get('share') 
         continue
+    shares.append(i.get('share'))
     if 'short' in i:
-		names.append(i.get('short'))
-	else:
+        names.append("{} {}%".format(i.get('short'), i.get('share')))
+    else:
         names.append(i.get('name'))
-	shares.append(i.get('share'))
-	if 'color' in i:
-		colors.append(i.get('color'))
-	else:
-        colorList.append("#000000")
+    if 'color' in i:
+        colors.append(i.get('color'))
+    else:
+        colors.append("#000000")
 
-names.append("below one")
-shares.append(below_One_sum)
+names.append("below one {:.2f}%".format(below_One_sum))
+shares.append("{:.2f}".format(below_One_sum))
 colors.append("#000000")
+x = range(0, len(shares))
+    
+#barplot
 
-source = ColumnDataSource(dict(x=names,y=shares, color=colors))
+def barplot():
 
-plot = figure( title = "volby 2017",
-			x_axis_label = "strany",
-			y_axis_label = "hlasy" )
+    source = ColumnDataSource(dict(x=x,y=shares, names = names, color=colors))
 
-plot.vbar(source=source, x='x', top='y', bottom=0, width=0.7, color='color')
+    plot = figure( title = "volby 2017",
+                    x_axis_label = "strany",
+                    y_axis_label = "hlasy" )
 
-labels = LabelSet(x='x', y='y', text='y', level='glyph',
-        x_offset=-13.5, y_offset=0, source=source, render_mode='canvas')
-plot.add_layout(labels)
+    plot.vbar(source=source, x='x', top='y', bottom=0, width=0.7, color='color', legend='names')
 
-show(plot)
+    labels = LabelSet(x='x', y='y', text='y', level='glyph',
+            x_offset=-13.5, y_offset=0, source=source, render_mode='canvas')
+    plot.add_layout(labels)
+
+    show(plot)
+
+#piechart
+
+def piechart():
+
+    shares2 = [0] + shares
+    percents = []
+    achieved = 0
+    for s in shares2:
+        achieved += (float(s) / 100)
+        percents.append(achieved)
+
+    source = ColumnDataSource(data={
+        'start':[p*2*pi for p in percents[:-1]],
+        'end':[p*2*pi for p in percents[1:]],
+        'color':colors,
+        'label': names,
+        'value': percents[:-1]
+    })
+
+    plot = figure(x_range=(-1,1), y_range=(-1,1))
+    plot.wedge(x=0, y=0, radius=1,
+            start_angle='start',
+            end_angle='end',
+            color='color',
+            legend='label',
+            source=source)
+
+    show(plot)
